798d6864d2aa2326fde52fd6c98a0ae835f62f92
Rajesh Panda <therajeshpanda@gmail.com>
2017-08-27 02:24:27 +0530

    Adding another endpoint for killing the process
    Going to be used for the android app.

--- YouPiCast.py
+++ YouPiCast.py
@@ -13,11 +13,19 @@ def PlayNow(url):
 	if pid > 0:
 		os.system('killall omxplayer.bin')
 	modUrl = 'https://youtube.com/watch?v='+url
-	cmdStr = 'omxplayer -o hdmi `youtube-dl -g -f best %s`' % (modUrl)
+	cmdStr = 'omxplayer -o hdmi `youtube-dl -g -f 18 %s` --blank' % (modUrl)
 	threads = Thread(target=run,args=(cmdStr,))
 	threads.daemon = True
 	threads.start()
 	return 'Added : Url - ' + url
+
+@app.route('/kill')
+def Kill():
+	pid = os.system('$(pidof omxplayer)')
+	if pid > 0:
+		os.system('killall omxplayer.bin')
+	return 'Killed'
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
